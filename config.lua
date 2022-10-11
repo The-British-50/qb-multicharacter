f3c85bba73bbbcb18ed6757b5ada47f964147b97
Smithy <96345069+Smitthyy@users.noreply.github.com>
2022-10-11 12:50:24 +0100

    Updated to live server version

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
-Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
-Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
+Config.Interior = vector3(-1079.68, -247.82, 44.02) -- Interior to load where characters are previewed
+Config.DefaultSpawn = vector3(427.3, -982.56, 30.71) -- Default spawn coords if you have start apartments disabled
+Config.PedCoords = vector4(377.39, -1584.91, 29.29, 317.96) -- Create preview ped at these coordinates
 Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
+Config.CamCoords = vector4(379.67, -1582.45, 29.29, 136.84) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 
 Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
